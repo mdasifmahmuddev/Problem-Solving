774ad1ce2565317879501ef42469c6ede84985cc
mdasifmahmuddev <md.asif.mahmud.dev@gmail.com>
2025-08-16 18:05:07 +0600

    problem solving

---
+++ HackerRank/print.js
@@ -0,0 +1,13 @@
+
+//md asif mahmud : 16 aug 2025 
+
+ function fullname(a,b){ 
+   
+    let asif ='asif';
+    let mahmud='mahmud';
+    let propername= 'full name is :' +asif +' ' +mahmud ; 
+    return(propername);
+
+ }
+
+ console.log(fullname());
